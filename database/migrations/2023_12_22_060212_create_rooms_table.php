19ec196bc60a50039574a2a5a986e9b1361a6a5a
Angelo Arcillas <angeloarcillas64@gmail.com>
2023-12-22 16:06:15 +0800

    Setup message + room + user

--- database/migrations/2023_12_22_060212_create_rooms_table.php
+++ database/migrations/2023_12_22_060212_create_rooms_table.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Models\Room;
-use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,12 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('messages', function (Blueprint $table) {
+        Schema::create('rooms', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(User::class)->constrained()->onDelete('cascade');
-            $table->foreignIdFor(Room::class)->constrained()->onDelete('cascade');
-
-            $table->string('body');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -29,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('messages');
+        Schema::dropIfExists('rooms');
     }
 };
